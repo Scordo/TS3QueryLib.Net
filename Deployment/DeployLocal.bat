445b738788e2edee5a89c0ba8021732ced7f1691
Scordo <admin@dev-com.de>
2017-09-08 17:22:21 +0200

    Changed version to 1.2.2

--- Deployment/DeployLocal.bat
+++ Deployment/DeployLocal.bat
@@ -1,3 +1,3 @@
 
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Deploy.target /target:Deploy
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" Deploy.target /target:Deploy /verbosity:m
 pause
