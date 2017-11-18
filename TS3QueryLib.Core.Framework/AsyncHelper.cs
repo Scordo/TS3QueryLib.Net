da80a32d5dcf6e9339ccf82f88bcf6c6a947edba
Jens Hofmann <admin@dev-com.de>
2017-11-18 11:49:37 +0100

    Changed .Net Framework to 4.5

---
+++ TS3QueryLib.Core.Framework/AsyncHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TS3QueryLib.Core
+{
+    public static class AsyncHelper
+    {
+        private static TaskFactory TaskFactory { get; } = new TaskFactory(CancellationToken.None, TaskCreationOptions.None, TaskContinuationOptions.None, TaskScheduler.Default);
+
+        public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+        {
+            return TaskFactory.StartNew(func).Unwrap().GetAwaiter().GetResult();
+        }
+
+        public static void RunSync(Func<Task> func)
+        {
+            TaskFactory.StartNew(func).Unwrap().GetAwaiter().GetResult();
+        }
+    }
+}
